81e7c60c0a7eebc17d873e48d2bbde77a11b9351
luisugaldecastrillo <luis.ugaldecastrillo@ucr.ac.cr>
2025-12-16 19:09:13 -0600

    Add method to update item quantity in cart

--- Controller/CarritoController.php
+++ Controller/CarritoController.php
@@ -36,6 +36,28 @@ class CarritoController {
         }
     }
 
+    public static function actualizarCantidad($id, $cantidad) {
+
+        if (!isset($_SESSION['carrito'][$id])) {
+            return;
+        }
+
+        $cantidad = (int)$cantidad;
+
+        if ($cantidad <= 0) {
+            unset($_SESSION['carrito'][$id]);
+            return;
+        }
+
+        $stock = (int)$_SESSION['carrito'][$id]['stock'];
+
+        if ($cantidad > $stock) {
+            $cantidad = $stock;
+        }
+
+        $_SESSION['carrito'][$id]['cantidad'] = $cantidad;
+    }
+
     public static function obtener() {
         return $_SESSION['carrito'] ?? [];
     }
@@ -56,3 +78,85 @@ class CarritoController {
         unset($_SESSION['carrito']);
     }
 }
+
+if (isset($_POST["btnAgregarCarrito"])) {
+
+    $producto = [
+        'id_producto' => $_POST["id_producto"],
+        'nombre'      => $_POST["nombre"],
+        'precio'      => $_POST["precio"],
+        'cantidad'    => isset($_POST["cantidad"]) ? (int)$_POST["cantidad"] : 1,
+        'stock'       => $_POST["stock"]
+    ];
+
+    CarritoController::agregar($producto);
+
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    } else {
+        header("Location: /RepoProyectoG5/View/Inicio/Principal.php");
+    }
+    exit;
+}
+
+if (isset($_POST["btnEliminarCarrito"])) {
+
+    $id = (int)$_POST["id_producto"];
+    CarritoController::eliminar($id);
+
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    } else {
+        header("Location: /RepoProyectoG5/View/Carrito/Carrito.php");
+    }
+    exit;
+}
+
+if (isset($_POST["btnSumarCarrito"])) {
+
+    $id = (int)$_POST["id_producto"];
+
+    $carrito = CarritoController::obtener();
+    if (isset($carrito[$id])) {
+        $nueva = $carrito[$id]["cantidad"] + 1;
+        CarritoController::actualizarCantidad($id, $nueva);
+    }
+
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    } else {
+        header("Location: /RepoProyectoG5/View/Carrito/Carrito.php");
+    }
+    exit;
+}
+
+if (isset($_POST["btnRestarCarrito"])) {
+
+    $id = (int)$_POST["id_producto"];
+
+    $carrito = CarritoController::obtener();
+    if (isset($carrito[$id])) {
+        $nueva = $carrito[$id]["cantidad"] - 1;
+        CarritoController::actualizarCantidad($id, $nueva);
+    }
+
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    } else {
+        header("Location: /RepoProyectoG5/View/Carrito/Carrito.php");
+    }
+    exit;
+}
+
+if (isset($_POST["btnVaciarCarrito"])) {
+
+    CarritoController::vaciar();
+
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    } else {
+        header("Location: /RepoProyectoG5/View/Carrito/Carrito.php");
+    }
+    exit;
+}
+
